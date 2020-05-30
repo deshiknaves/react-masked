44df9078af61ca49bf9ffeb1d78a2a2f3178798f
Deshi Rahim <nawal.rahim@gmail.com>
2020-05-31 09:42:23 +1000

    ✅ Add tests for match

---
+++ src/match.test.js
@@ -0,0 +1,88 @@
+import { matchedValue, autoFillCharacters } from './match'
+
+describe('Match', () => {
+  describe(matchedValue.name, () => {
+    const createOptions = (options = {}) => ({
+      autoCharacters: [],
+      reverse: false,
+      ...options,
+    })
+
+    it("should not match if value doesn't match", () => {
+      const [match, value] = matchedValue(
+        createOptions({
+          mask: /^\d{2}$/,
+          value: 'AB',
+          validExample: '00',
+        }),
+      )
+
+      expect(match).toBeFalsy()
+      expect(value).toBe('AB')
+    })
+
+    it('should be able to partially match a string', () => {
+      const [match, value] = matchedValue(
+        createOptions({
+          mask: /^\d{2}$/,
+          value: '0',
+          validExample: '00',
+        }),
+      )
+
+      expect(match).toBeTruthy()
+      expect(value).toBe('0')
+    })
+
+    it('should not match if value exceeds the regex', () => {
+      const [match, value] = matchedValue(
+        createOptions({
+          mask: /^\d{2}$/,
+          value: '0000',
+          validExample: '00',
+        }),
+      )
+
+      expect(match).toBeFalsy()
+      expect(value).toBe('0000')
+    })
+  })
+
+  describe(autoFillCharacters.name, () => {
+    it('should be able to add characters that are auto fillable', () => {
+      const added = autoFillCharacters({
+        autoCharacters: ['/'],
+        remaining: '/12',
+      })
+
+      expect(added).toEqual('/')
+    })
+
+    it('should be able to add more than one characters that are auto fillable', () => {
+      const added = autoFillCharacters({
+        autoCharacters: ['/'],
+        remaining: '//12',
+      })
+
+      expect(added).toEqual('//')
+    })
+
+    it('should be able to add more than one characters that are different that are auto fillable', () => {
+      const added = autoFillCharacters({
+        autoCharacters: ['/', '_'],
+        remaining: '/_12',
+      })
+
+      expect(added).toEqual('/_')
+    })
+
+    it('should not auto fill characters if they are not the next character', () => {
+      const added = autoFillCharacters({
+        autoCharacters: ['/'],
+        remaining: '0/12',
+      })
+
+      expect(added).toEqual('')
+    })
+  })
+})
