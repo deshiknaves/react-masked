626e380e2ff181529b80b2297c77366d06c55744
Deshi Rahim <nawal.rahim@gmail.com>
2020-06-07 13:39:23 +1000

    📝 More complicated date example

--- src/Examples.tsx
+++ src/Examples.tsx
@@ -6,13 +6,32 @@ import { Check } from './Check'
 import './Examples.css'
 import { matchDate } from './custom'
 
+const placeholderColor = '#c1c1c1'
+
+const dateMatcher = matchDate('dd/mm/yyyy')
+
+const yearFour = /^\d{1,2}\/\d{1,2}\/\d{4}$/
+const yearTwo = /^\d{1,2}\/\d{1,2}\/\d{2}$/
+const yearTwoPlaceholder = 'dd/mm/yy'
+const yearFourPlaceholder = 'dd/mm/yyyy'
+const yearTwoExample = '01/01/20'
+const yearFourExample = '01/01/2002'
+
 function App() {
   const [basicComplete, setBasicComplete] = useState(false)
   const [americanComplete, setAmericanComplete] = useState(false)
   const [britishComplete, setBritishComplete] = useState(false)
+  const [britishVariableComplete, setBritishVariableComplete] = useState(false)
+  const [britishVariable, setBritishVariable] = useState({
+    mask: yearTwo,
+    placeholder: yearTwoPlaceholder,
+    example: yearTwoExample,
+    length: 2,
+  })
 
   return (
     <main className="app">
+      <h1>ReactMasked Eamples</h1>
       <section>
         <h2>Basic</h2>
         <p>Just a basic regular expressions</p>
@@ -22,7 +41,7 @@ function App() {
             mask={/^[a-zA-Z]{2}-\d{3}$/}
             validExample="AA-000"
             placeholder="AA-DDD"
-            placeholderColor="#c1c1c1"
+            placeholderColor={placeholderColor}
             onChange={(_, complete) => setBasicComplete(complete)}
           >
             {maskedProps => <Input {...maskedProps} />}
@@ -33,14 +52,16 @@ function App() {
 
       <section>
         <h2>Date American</h2>
-        <p>Custom matcher to match a date (mm/dd/yyyy)</p>
+        <p>
+          Custom matcher to match a date <strong>mm/dd/yyyy</strong>
+        </p>
         <div className="field">
           <MaskedInput
             autoCharacters={['/']}
             mask={/^\d{1,2}\/\d{1,2}\/\d{4}$/}
             validExample="01/01/2000"
             placeholder="mm/dd/yyyy"
-            placeholderColor="#c1c1c1"
+            placeholderColor={placeholderColor}
             onMatch={matchDate('mm/dd/yyyy')}
             onChange={(_, complete) => setAmericanComplete(complete)}
           >
@@ -52,20 +73,62 @@ function App() {
 
       <section>
         <h2>Date Everywhere else</h2>
-        <p>Custom matcher to match a date (dd/mm/yyyy)</p>
+        <p>
+          Custom matcher to match a date <strong>dd/mm/yyyy</strong>
+        </p>
         <div className="field">
           <MaskedInput
             autoCharacters={['/']}
             mask={/^\d{1,2}\/\d{1,2}\/\d{4}$/}
             validExample="01/01/2000"
             placeholder="dd/mm/yyyy"
-            placeholderColor="#c1c1c1"
-            onMatch={matchDate('dd/mm/yyyy')}
+            placeholderColor={placeholderColor}
+            onMatch={dateMatcher}
             onChange={(_, complete) => setBritishComplete(complete)}
           >
             {maskedProps => <Input {...maskedProps} />}
           </MaskedInput>
           {britishComplete && <Check />}
+        </div>
+      </section>
+
+      <section>
+        <h2>2 or 4 digit year</h2>
+        <p>
+          Custom matcher to match a date with <strong>dd/mm/(yy|yyyy)</strong>
+        </p>
+        <div className="field">
+          <MaskedInput
+            autoCharacters={['/']}
+            mask={britishVariable.mask}
+            validExample={britishVariable.example}
+            placeholder={britishVariable.placeholder}
+            placeholderColor={placeholderColor}
+            onMatch={values => {
+              const length = values.value.length > 8 ? 4 : 2
+              if (length === britishVariable.length) return dateMatcher(values)
+
+              const four = length === 4
+              const remaining = four
+                ? yearFourExample.substring(values.value.length)
+                : yearTwoExample.substring(values.value.length)
+              const mask = four ? yearFour : yearTwo
+              const match = dateMatcher({ ...values, mask, remaining })
+
+              setBritishVariable({
+                mask,
+                placeholder: four ? yearFourPlaceholder : yearTwoPlaceholder,
+                example: four ? yearFourExample : yearTwoExample,
+                length,
+              })
+
+              return match
+            }}
+            onChange={(_, complete) => setBritishVariableComplete(complete)}
+          >
+            {maskedProps => <Input {...maskedProps} />}
+          </MaskedInput>
+          {britishVariableComplete && <Check />}
         </div>
       </section>
     </main>
